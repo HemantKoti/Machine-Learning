ee591dcda552ddb05f800edd42462ba2d61613f7
Hemant Koti <Hemant Koti>
2017-08-21 19:12:27 +0530

    Completed Data Preprocessing

--- Udemy - Machine Learning A to Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.R
+++ Udemy - Machine Learning A to Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing.R
@@ -11,3 +11,27 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary), 
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country, 
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased, 
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
+                          
+# splitting the data into training set and test set
+
+# install.packages('caTools') # installs caTools library
+# library(caTools) # select the library caTools
+
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+#factors in R cannot be scaled, hence we use only 2 (age) and 3 (salary) columns to scale
+training_set[, 2 : 3] = scale(training_set[, 2 : 3])
+test_set[, 2 : 3] = scale(test_set[, 2 : 3])
