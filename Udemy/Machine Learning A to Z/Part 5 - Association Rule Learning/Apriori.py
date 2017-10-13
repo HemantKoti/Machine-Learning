78ec1332c31a7fa3bfb7ad6d9ce74911e966633d
Hemant <Koti.Hemant@unisys.com>
2017-10-13 09:55:16 +0530

    Classification

--- Udemy/Machine Learning A to Z/Part 5 - Association Rule Learning/Apriori.py
+++
@@ -1,9 +0,0 @@
-# Apriori
-
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
--- Udemy/Machine Learning A to Z/Part 5 - Association Rule Learning/Section 29 - Eclat/eclat.R
+++ Udemy/Machine Learning A to Z/Part 5 - Association Rule Learning/Section 29 - Eclat/eclat.R
@@ -1,8 +1,8 @@
 # Eclat
 
 # Data Preprocessing
-# install.packages('arules')
-# library(arules)
+install.packages('arules')
+library(arules)
 dataset = read.csv('Market_Basket_Optimisation.csv')
 dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
