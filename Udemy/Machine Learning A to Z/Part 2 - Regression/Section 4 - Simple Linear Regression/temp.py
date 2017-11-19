d2643291c59b6fca56e16fb5bc88f21a668575d3
Hemant <Koti.Hemant@unisys.com>
2017-11-20 01:17:07 +0530

    LatestChanges

---
+++ Udemy/Machine Learning A to Z/Part 2 - Regression/Section 4 - Simple Linear Regression/temp.py
@@ -0,0 +1,23 @@
+from pandas import Series
+from statsmodels.tsa.arima_model import ARIMA
+import numpy
+
+# create a differenced series
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return numpy.array(diff)
+
+# load dataset
+series = Series.from_csv('dataset.csv', header=None)
+# seasonal difference
+X = series.values
+days_in_year = 365
+differenced = difference(X, days_in_year)
+# fit model
+model = ARIMA(differenced, order=(7,0,1))
+model_fit = model.fit(disp=0)
+# print summary of fit model
+print(model_fit.summary())
